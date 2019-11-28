5cf6d64a87c4f78f91dfa079e10973392738d125
Moppit <michelletranusa@gmail.com>
2019-11-28 00:00:39 -0700

    Finished application supplement for CodePath class

--- AppForCodePath/ViewController.swift
+++ AppForCodePath/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    var originalBackgroundColor: UIColor!
+    @IBOutlet weak var addCustomTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Store the original background color
+        originalBackgroundColor = view.backgroundColor
     }
 
-
+    @IBAction func changeTextColor(_ sender: UIButton) {
+        print("Hello")
+        textLabel.textColor = UIColor.orange
+    }
+    
+    @IBAction func changeViewButton(_ sender: UIButton) {
+        view.backgroundColor = UIColor.blue
+    }
+    
+    @IBAction func changeTextStringButton(_ sender: UIButton) {
+        if(addCustomTextField.text == "")
+        {
+            textLabel.text = "...and I say hello!"
+        }
+        else
+        {
+            textLabel.text = addCustomTextField.text
+        }
+        view.endEditing(true)
+        addCustomTextField.text = ""
+    }
+    
+    @IBAction func resetView(_ sender: Any) {
+        textLabel.text = "You say goodbye..."
+        textLabel.textColor = UIColor.black
+        view.backgroundColor = originalBackgroundColor
+    }
+    
+    
 }
 
